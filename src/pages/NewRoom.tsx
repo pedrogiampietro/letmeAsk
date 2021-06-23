02928ea84ad4f4400c1c078bae237cec9c66f9e0
Pedro Giampietro <pedrogiampietro@hotmail.com>
2021-06-23 03:51:43 -0300

    Criado Rotas e navegação

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
@@ -19,10 +20,10 @@ export function NewRoom() {
           <form>
             <input type="text" placeholder="Nome da sala" />
 
-            <Button type="submit">criar sala</Button>
+            <Button type="submit">Criar sala</Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
           </p>
         </div>
       </main>
